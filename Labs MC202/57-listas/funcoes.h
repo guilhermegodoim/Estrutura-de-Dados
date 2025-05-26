d3cfd6c9042995ac7268063bb8e3eae802eac77a
Guilherme Godoi <g237031@dac.unicamp.br>
2025-05-26 19:32:35 -0300

    script

---
+++ Labs MC202/57-listas/funcoes.h
@@ -0,0 +1,8 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include<stdlib.h>
+#include<stdio.h>
+
+
+#endif
---
+++ Labs MC202/57-listas/listas.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include "funcoes.h"
+
+int main() {
+    printf("Projeto: 57-listas\n");
+    
+    return 0;
+}
